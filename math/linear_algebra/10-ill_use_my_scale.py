1cf3f2b40845e4527e38ae3b211ca9bafc93e0f2
buterajacques1 <jbutera702@gmail.com>
2023-09-10 18:12:38 +0200

    I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Function that calculates the shape of a matrix"""
+
+
+def np_shape(matrix):
+    """Find the shape of a matrix"""
+    import numpy as np
+    shape = np.shape(matrix)
+    return shape
