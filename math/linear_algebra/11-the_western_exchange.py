6c75b8b8786931c87df54a874fe283e572b6429a
buterajacques1 <jbutera702@gmail.com>
2023-09-10 18:17:12 +0200

    The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Function that transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """Transposes a matrix"""
+    import numpy as np
+    t = np.transpose(matrix)
+    return t
