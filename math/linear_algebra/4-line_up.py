9901f0a9d5fe05c087801bc1451742919c4e2366
buterajacques1 <jbutera702@gmail.com>
2023-09-10 16:05:17 +0200

    Line Up

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 
-"""Function that adds two arrays element-wise """
-
-
+"""Function that adds two arrays element-wise"""
 
 def add_arrays(arr1, arr2):
-    "check if the arrays have the same lenght"
+    """Check if the arrays have the same length"""
     if len(arr1) != len(arr2):
         return None
-    """initialize new array"""
+
+    """Initialize a new array"""
     newarr = []
 
-    '''Iterate through arrays and add element-wise'''
-
+    """Iterate through arrays and add element-wise"""
     for elem1, elem2 in zip(arr1, arr2):
         newarr.append(elem1 + elem2)
 
     return newarr
-
