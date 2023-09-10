1528d7e49bcdb730e92fc49ac6dec4ac3cb98d3e
buterajacques1 <jbutera702@gmail.com>
2023-09-10 18:40:22 +0200

    Slice Like A Ninja

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
-
-"""FUnction that adds two matrices"""
-
-
 def add_matrices(mat1, mat2):
-    """Check if mat1 and mat2 have the same shape"""
-    if mat1.shape != mat2.shape:
+    # Check if the matrices have the same shape
+    if not isinstance(mat1, list) or not isinstance(mat2, list):
+        return None
+    if len(mat1) != len(mat2):
         return None
 
-    # Perform element-wise addition of the two matrices
-    result = mat1 + mat2
+    # Initialize the result matrix with the same shape as mat1 and mat2
+    result = []
+
+    # Iterate through the rows of mat1 and mat2
+    for i in range(len(mat1)):
+        # Check if the rows have the same shape
+        if len(mat1[i]) != len(mat2[i]):
+            return None
+
+        # Initialize the row of the result matrix
+        row = []
+
+        # Iterate through the elements of the rows and add them element-wise
+        for j in range(len(mat1[i])):
+            row.append(mat1[i][j] + mat2[i][j])
+
+        # Add the row to the result matrix
+        result.append(row)
 
     return result
