148aaeed80b88531fb91031d9b1a650ece95e78e
buterajacques1 <jbutera702@gmail.com>
2024-03-20 10:16:23 +0200

    What will be next?

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
-""" Script for getting SpaceX launch info"""
+
+"""
+This module contains a function that
+uses Uonofficial SpaceX API to display
+upcoming launch with these information:
+    - Name of the launch
+    - Date (in local time)
+    - Rocket name
+    - Launchpad name
+format of the output:
+<launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
+"""
+
 import requests
 
 
-if __name__ == '__main__':
-    url = 'https://api.spacexdata.com/v4/launches/upcoming'
-    response = requests.get(url).json()
-    date = [x['date_unix'] for x in response]
-    idx = date.index(min(date))
-    launch = response[idx]
-    launch_name = launch['name']
-    date_l = launch['date_local']
+def upcomingLaunch():
+    """print upcoming launch"""
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    response = requests.get(url)
+    data = response.json()
+    launch = data[0]
     rocket_id = launch['rocket']
+    launchpad_id = launch['launchpad']
     rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    rocket_name = requests.get(rocket_url).json()['name']
-    lpad_id = launch['launchpad']
-    lpad_url = "https://api.spacexdata.com/v4/launchpads/{}".\
-        format(lpad_id)
-    lpad_req = requests.get(lpad_url).json()
-    lpad_name = lpad_req['name']
-    lpad_loc = lpad_req['locality']
+    launchpad_url = "https://api.spacexdata.com/v4/launchpads/{}".format(
+        launchpad_id)
+    rocket_response = requests.get(rocket_url)
+    launchpad_response = requests.get(launchpad_url)
+    rocket_data = rocket_response.json()
+    launchpad_data = launchpad_response.json()
+    rocket_name = rocket_data['name']
+    launchpad_name = launchpad_data['name']
+    launchpad_locality = launchpad_data['locality']
+    print("{} ({}) {} - {} ({})".format(
+        launch['name'],
+        launch['date_local'],
+        rocket_name,
+        launchpad_name,
+        launchpad_locality
+    ))
 
-    upcoming_launch = "{} ({}) {} - {} ({})".format(launch_name, date_l,
-                                                    rocket_name, lpad_name,
-                                                    lpad_loc)
 
-    print(upcoming_launch)
+if __name__ == "__main__":
+    upcomingLaunch()
