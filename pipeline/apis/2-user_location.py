51bc3c1dac51da6e71831d13fd1ad0a611c50b6a
buterajacques1 <jbutera702@gmail.com>
2024-03-20 09:05:01 +0200

    Rate me is you can!

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -4,31 +4,24 @@ Prints the location of a specific Github user
 """
 import requests
 import sys
-from datetime import datetime
+import time
 
-def get_user_location(url):
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        location = response.json().get('location', 'Not available')
-        if location:
-            print(location)
-        else:
-            print('Not available')
-
-    elif response.status_code == 404:
-        print("Not found")
-
-    elif response.status_code == 403:
-        rate_limit_reset = int(response.headers.get('X-Ratelimit-Reset', 0))
-        reset_time = datetime.fromtimestamp(rate_limit_reset)
-        minutes = (reset_time - datetime.now()).total_seconds() / 60
-        print(f"Reset in {int(minutes)} min")
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        get_user_location(sys.argv[1])
-    else:
-        print("Usage: ./2-user_location.py <URL>")
+    if len(sys.argv) != 2:
+        exit()
+    url = sys.argv[1]
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers)
 
+    if r.status_code == 200:
+        print(r.json()['location'])
+
+    if r.status_code == 404:
+        print("Not found")
+
+    if r.status_code == 403:
+        rate_limit = int(r.headers['X-Ratelimit-Reset'])
+        now = int(time.time())
+        minutes = int((rate_limit - now) / 60)
+        print("Reset in {} min".format(minutes))
