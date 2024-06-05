b024f653c9ca8d6916329d97dc4dbb207e3eeace
buterajacques1 <jbutera702@gmail.com>
2024-06-05 14:43:14 +0200

    RNN Encoder

---
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+class RNNEncoder(tf.keras.layers.Layer):
+    def __init__(self, vocab, embedding, units, batch):
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
+
+if __name__ == "__main__":
+    import numpy as np
+
+    encoder = RNNEncoder(1024, 128, 256, 32)
+    print(encoder.batch)
+    print(encoder.units)
+    print(type(encoder.embedding))
+    print(type(encoder.gru))
+
+    initial = encoder.initialize_hidden_state()
+    print(initial)
+    x = tf.convert_to_tensor(np.random.choice(1024, 320).reshape((32, 10)))
+    outputs, hidden = encoder(x, initial)
+    print(outputs)
+    print(hidden)
