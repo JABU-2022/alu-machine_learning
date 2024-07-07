eb619f5b1e3377783473df45b64f03b794e7e0a8
buterajacques1 <jbutera702@gmail.com>
2024-07-07 21:26:22 +0200

    Absorbing

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,39 +1,50 @@
-#!/usr/bin/env python3
-""" Absorbing Markov chain"""
-
 import numpy as np
-
 
 def absorbing(P):
     """
-    Function that determines if a markov chain is absorbing
+    Function that determines if a Markov chain is absorbing
     Args:
         P: 2D numpy.ndarray of shape (n, n) representing the transition matrix
            P[i, j]: is the probability of transitioning from state i to state j
-           n: the number of states in the markov chain
+           n: the number of states in the Markov chain
     Returns: True if it is absorbing, or False on failure
     """
     if len(P.shape) != 2:
-        return None
+        return False
     n1, n2 = P.shape
-    if (n1 != n2) or type(P) is not np.ndarray:
-        return None
+    if (n1 != n2) or not isinstance(P, np.ndarray):
+        return False
+    
     D = np.diagonal(P)
     if (D == 1).all():
         return True
     if not (D == 1).any():
         return False
-
-    # formula t=(I-Q)^-1 1
-    # approach using Neumman series: t = (∑ inf k=0 * Q^k) * 1
-    # suggested implementation based on: https://stackoverflow.com/
-    # questions/45164505/best-iterative-way-to-calculate-the-fundamental-matrix-
-    # of-an-absorbing-markov-ch
+    
+    # Identifying absorbing states
+    absorbing_states = np.where(D == 1)[0]
+    
+    # If there are no absorbing states, return False
+    if len(absorbing_states) == 0:
+        return False
+    
+    # Constructing the reachability matrix
+    reachability = np.linalg.matrix_power(P, n1)
+    
+    # Checking if every state can reach an absorbing state
     for i in range(n1):
-        # print('this is Pi {}'.format(P[i]))
-        for j in range(n2):
-            # print('this is Pj {}'.format(P[j]))
-            if (i == j) and (i + 1 < len(P)):
-                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
-                    return False
+        if not any(reachability[i, absorbing_states] > 0):
+            return False
+    
     return True
+
+# Example usage:
+P = np.array([
+    [0.5, 0.5, 0, 0],
+    [0.2, 0.3, 0.5, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0.2, 0.8]
+])
+
+print(absorbing(P))  # Output should be True
+
